75eaa863a0667a2c22eaa6cee1e0ac6e3c7d113d
Abran21402 <abranabru@gmail.com>
2025-01-26 12:37:48 +0530

    added login and register page

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './components/LandingPage/landing-page/landing-page.component';
+import { RegistrationComponent } from './components/LandingPage/registration/registration.component';
+import { LoginComponent } from './components/LandingPage/login/login.component';
 
 export const routes: Routes = [
     {path:'',component:LandingPageComponent},
+    { path: 'register', component: RegistrationComponent },
+    { path: 'login', component: LoginComponent },
 ];
