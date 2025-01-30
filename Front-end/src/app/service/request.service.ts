fb17dd8dd5b98c07d26abe1a81b8782e447316e2
Sumanth-2002 <thotamsettysumanth@gmail.com>
2025-01-30 11:00:54 +0530

    admin-requests

--- Front-end/src/app/service/request.service.ts
+++ Front-end/src/app/service/request.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root', // Makes this service available app-wide
 })
 export class RequestService {
-
-  constructor() { }
+  private apiUrl = 'http://localhost:9092/api/stores'; // Update with actual API URL
+  constructor(private http: HttpClient) {}
+  // Fetch pending requests from the API
+  getPendingRequests(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.apiUrl}/get-requests/COMP14652A`);
+  }
+ 
+  updateRequestStatus(updatedRequest: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/update-request`, updatedRequest);
+  }
 }
