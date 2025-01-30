6bd47d543effa80a3c333f2a9aaa4b876296f029
siva <sivaravinadhan@gmail.com>
2025-01-30 22:29:12 +0530

    Added Charts for Admin Dashboard

---
+++ Front-end/src/app/service/metrics.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MetricsService {
+  private apiUrl = 'http://localhost:9095/getTotalProductsSelled';
+
+  constructor(private http:HttpClient) {}
+
+  getProductSalesByStore(companyId: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${companyId}`);
+  }
+}
