97853b9d452686a4dba815ae47e639f1055d447d
Sumanth-2002 <thotamsettysumanth@gmail.com>
2025-01-30 12:15:58 +0530

    leaderboard

--- Front-end/src/app/components/StoreDashboard/store-leaderboard/store-leaderboard.component.ts
+++ Front-end/src/app/components/StoreDashboard/store-leaderboard/store-leaderboard.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { HeaderComponent } from '../header/header.component';
 import { SidebarComponent } from '../side-bar/side-bar.component';
-
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { HeaderComponent } from '../header/header.component';
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-store-leaderboard',
-  imports: [HeaderComponent,SidebarComponent],
+  imports: [HeaderComponent,SidebarComponent,CommonModule],
+  
   templateUrl: './store-leaderboard.component.html',
   styleUrl: './store-leaderboard.component.css'
 })
 export class StoreLeaderboardComponent {
-
+  leaderboardData: any[] = [];
+  constructor(private http: HttpClient) {}
+  ngOnInit(): void {
+    this.fetchLeaderboardData();
+  }
+  fetchLeaderboardData(): void {
+    // Replace with your backend API endpoint
+    const apiUrl = 'http://localhost:9095/api/billing/get-leaderboard/STOR28116A';
+    this.http.get<any[]>(apiUrl).subscribe(
+      (data) => {
+        this.leaderboardData = data;
+      },
+      (error) => {
+        console.error('Error fetching leaderboard data:', error);
+      }
+    );
+  }
 }
